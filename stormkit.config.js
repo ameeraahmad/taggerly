18189edb92dd0c65bce8f6011f9050865fe52741
ameeraahmad <ameeraahmad511@gmail.com>
2026-03-28 01:03:11 +0200

    Testing redirects

--- stormkit.config.js
+++ stormkit.config.js
@@ -4,20 +4,16 @@ module.exports = {
   },
   redirects: [
     {
-      from: "/api/(.*)",
-      to: "/server.js",
+      from: "/test",
+      to: "/login.html",
+    },
+    {
+      from: "/api/:path*",
+      to: "server.js",
     },
     {
       from: "/ad-details.html",
-      to: "/server.js",
-    },
-    {
-      from: "/sitemap.xml",
-      to: "/server.js",
-    },
-    {
-      from: "/robots.txt",
-      to: "/server.js",
+      to: "server.js",
     }
   ]
 };
