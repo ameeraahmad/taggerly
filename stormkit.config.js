0b245f1e4832556f39a8bf3848a7423620c8ac95
ameeraahmad <ameeraahmad511@gmail.com>
2026-03-28 00:06:57 +0200

    fix: cleanup stormkit config

--- stormkit.config.js
+++ stormkit.config.js
@@ -1,28 +1,5 @@
 module.exports = {
   server: {
     entry: "server.js",
-  },
-  redirects: [
-    {
-      from: "/api/(.*)",
-      to: "server.js",
-      method: "rewrite",
-    },
-    {
-      from: "/ad-details.html",
-      to: "server.js",
-      method: "rewrite",
-    },
-    {
-      from: "/sitemap.xml",
-      to: "server.js",
-      method: "rewrite",
-    },
-    {
-      from: "/robots.txt",
-      to: "server.js",
-      method: "rewrite",
-    }
-  ]
-
+  }
 };
